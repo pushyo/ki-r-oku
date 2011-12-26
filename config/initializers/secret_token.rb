44aed4342cc05ce031be5e48ef2c98f0619b09a6
yoann estanove <yoann.estanove@gmail.com>
2011-12-26 15:18:51 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kiroku::Application.config.secret_token = 'd8713d6c487d0563edef0949510e474511b57b318f64067275065b6f9da948c3d5f31cb1a53f34dedcd4a7ec7b2efe3f4856ce555e7f8803b98e156bf543416a'
