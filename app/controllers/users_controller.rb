49a791e859c0829a88f963f583691652999d3494
yoann estanove <yoann.estanove@gmail.com>
2011-12-31 13:34:35 +0800

    operational signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,13 @@ class UsersController < ApplicationController
     @title = "Sign up"
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
   attr_accessor :password
-  attr_accessible :name, :email, :password, :password_confirmation
+  #attr_accessible :name, :email, :password, :password_confirmation
   # Automatically create the virtual password 'password_confirmation'
+
+  email_regex = /\A[a-z\d][\w+\-.]*@[a-z\d\-.]+\.[a-z]{2,4}\z/i
+
+  validates :name,  :presence => true,
+                    :length   => { :maximum => 50 }
+  validates :email, :presence => true,
+                    :format   => { :with => email_regex } 
   validates :password, :presence      => true,
                        :confirmation  => true,
                        :length        => { :within => 6..40 }
