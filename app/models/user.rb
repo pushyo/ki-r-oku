e5ccb0a7e9f4a80ee77f634017e0ec26dabbe138
yoann estanove <yoann.estanove@gmail.com>
2012-01-02 17:48:49 +0800

    registration working with ActiveModel::SecurePassword

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,14 @@
 class User < ActiveRecord::Base
-  attr_accessor :password
-  #attr_accessible :name, :email, :password, :password_confirmation
-  # Automatically create the virtual password 'password_confirmation'
+  # introduced in Rails 3.1, needs gem bcrypt-ruby, needs password_digest column,
+  # provides password and password_confirmation
+  has_secure_password
 
   email_regex = /\A[a-z\d][\w+\-.]*@[a-z\d\-.]+\.[a-z]{2,4}\z/i
 
-  validates :name,  :presence => true,
-                    :length   => { :maximum => 50 }
-  validates :email, :presence => true,
-                    :format   => { :with => email_regex } 
-  validates :password, :presence      => true,
-                       :confirmation  => true,
-                       :length        => { :within => 6..40 }
+  validates :name,      :presence => true,
+                        :length   => { :maximum => 50 }
+  validates :email,     :presence => true,
+                        :format   => { :with => email_regex } 
+  validates :password,  :length   => { :within => 6..40 }
 
 end
